8be553dd363d8f999218e06870839eebe47dadff
maurosoria <maurosoria@gmail.com>
2014-09-04 20:38:00 -0300

    Install bs4 help

--- s3arch.py
+++ s3arch.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 import urllib.parse
 import sys
+try:
+        from bs4 import BeautifulSoup
+except:
+        print ("Need BeautifulSoup4 to run")
+        print ("pip3 install BeautifulSoup4")
+        print ("or")
+        print ("Download from http://www.crummy.com/software/BeautifulSoup/#Download && python3 setup.py install")
+        sys.exit (1)
 from lib.search import *
 from lib.core.Controller import Controller
 from lib.core.ArgumentsParser import ArgumentsParser
-
 
 class Program:
 	def __init__(self):
